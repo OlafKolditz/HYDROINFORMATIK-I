d7fc2440aa944aade301bdb43fe57072b3a08abf
Olaf Kolditz <olaf.kolditz@ufz.de>
2020-11-05 20:48:29 +0100

    Add files via upload

---
+++ EX01-main-function.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  std::cout << "Hallo C++ World, heute ist der 06.11.2020 und die US-Präsidenten-Wahl ist immer noch nicht entschieden ..." << std::endl;
+  return 0;
+}
+
