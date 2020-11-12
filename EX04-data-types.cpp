6bd142096b32a398c05fdbb450bb55f209c7bff4
Olaf Kolditz <olaf.kolditz@ufz.de>
2020-11-12 23:14:00 +0100

    Add files via upload
    c++ example for size of data types

---
+++ EX04-data-types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "E323: Size of data types\n";
+//  system("pwd");
+  cout << "Type\tNumber of bytes\n";
+  cout << "-----------------------\n";
+  cout << "bool\t\t" << sizeof(bool) << endl;
+  cout << "char\t\t" << sizeof(char) << "\n";
+  cout << "short\t\t" << sizeof(short) << endl;
+  cout << "int\t\t" << sizeof(int) << endl;
+  cout << "long\t\t" << sizeof(long) << endl;
+  cout << "float\t\t" << sizeof(float) << endl;
+  cout << "double\t\t" << sizeof(double) << endl;
+  cout << "long double\t" << sizeof(long double) << endl;
+  return 0;
+}
+
