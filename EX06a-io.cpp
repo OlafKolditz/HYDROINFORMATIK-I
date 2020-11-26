42d795daf39fe86f5c7e01cf2c354096f5321cdd
Olaf Kolditz <olaf.kolditz@ufz.de>
2020-11-26 23:14:56 +0100

    Add files via upload
    standard input-output instructions

---
+++ EX06a-io.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  long zahl;
+  cout << "Bitte eine ganze Zahl eingeben: ";
+  cin >> zahl;
+  cout << "Die eingegebene Zahl war: " << zahl << endl << "Zeilenumbruch";
+  return 0;
+}
+
