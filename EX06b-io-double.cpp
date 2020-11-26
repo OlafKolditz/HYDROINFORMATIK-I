775dfbdf9eafedc6369b3b79f052d6d32ecfe83c
Olaf Kolditz <olaf.kolditz@ufz.de>
2020-11-26 23:19:35 +0100

    Add files via upload
    io of double with different formats

---
+++ EX06b-io-double.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double zahl;
+  cout << "Bitte eine Gleitkommazahl eingeben: ";
+  cin >> zahl;
+
+  cout.precision(8);
+
+  cout << "Standard: \t"               << zahl << endl;
+  cout << "showpoint: \t"  << showpoint  << zahl << endl;
+  cout << "fixed: \t\t"    << fixed      << zahl << endl;
+  cout << "scientific: \t" << scientific << zahl << endl;
+  return 0;
+}
+
