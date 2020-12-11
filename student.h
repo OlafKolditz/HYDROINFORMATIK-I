d8cf0762b9e76d6d34c4fc81be7290919f237660
Olaf Kolditz <olaf.kolditz@ufz.de>
2020-12-11 09:15:39 +0100

    changes in students.h

--- student.h
+++ student.h
@@ -1,19 +1,29 @@
 #include <string>
+#include <iostream>
+#include <fstream> 
 using namespace std;
-
 // Class definition
 class CStudent
 {
-//data - properties
-public:
+  private:
+    int year;
+    long bank_account;
+  //protected:
+  public:
     string name_first;
     string name_last;
-//methods - functionen
+    string course;
+    string birthday;
+    string nation;
+    string gender;
+    long id;
+    string email;
+    string city;
+    string street;
+    int bmp_id;
   public:
     CStudent();  // constructor
     ~CStudent(); // destructor
-  protected:
-  private:
+    ios::pos_type Read(ifstream&);
 };
-
 
