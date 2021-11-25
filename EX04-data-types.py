295bc2471d8a4200013ef438cbd451e4fd61d8fe
Olaf Kolditz <olaf.kolditz@ufz.de>
2021-11-25 23:50:25 +0100

    python version for data types

---
+++ EX04-data-types.py
@@ -0,0 +1,26 @@
+#boolean
+d = True
+print(d)
+type(d)
+print(type(d))
+print(d .__sizeof__())
+#integer
+a = 3
+print(a)
+type(a)
+print(type(a))
+print(a .__sizeof__())
+#float
+b = 3.1415
+print(b)
+type(b)
+print(type(b))
+print(b .__sizeof__())
+#string
+c = 'Hydroinformatik'
+print(c)
+type(c)
+print(type(c))
+id(c)
+print(id(c))
+print(c .__sizeof__())
